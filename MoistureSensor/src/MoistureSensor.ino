73fad8d3890bfeea732474f7a1a7799821a6d8a3
duthi008 <duthi008@umn.edu>
2021-12-02 17:03:33 -0600

    1

--- MoistureSensor/src/MoistureSensor.ino
+++ MoistureSensor/src/MoistureSensor.ino
@@ -5,14 +5,26 @@
  * Date:
  */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
 
+#define moistureSensor A0
+#define solenoid D3
+int moistureData;
+
+
+void setup() {
+  pinMode(moistureSensor, INPUT);
+  Particle.variable("Moisture", moisture);
+  Particle.function("Water", water);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
+  moistureData = analogRead(moistureSensor);
+
+}
+
+void water(String input){
+  
+
 
 }
