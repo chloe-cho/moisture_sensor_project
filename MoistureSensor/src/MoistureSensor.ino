03a0a5acc91db867a6c0a54ff0f39b41a020a9e5
duthi008 <duthi008@umn.edu>
2021-12-12 19:05:08 -0600

    Made basic Webpage and code for watering

--- MoistureSensor/src/MoistureSensor.ino
+++ MoistureSensor/src/MoistureSensor.ino
@@ -7,24 +7,41 @@
 
 
 #define moistureSensor A0
-#define solenoid D3
+#define pump D3
+
+#include <string>
+#include <stdlib.h>
+
 int moistureData;
 
 
 void setup() {
-  pinMode(moistureSensor, INPUT);
-  Particle.variable("Moisture", moisture);
-  Particle.function("Water", water);
+  pinMode(moistureSensor, INPUT); //pin setup for moisture sensor
+  pinMode(pump, OUTPUT); //pin setup for the motor wired with a transistor
+
+
+  Particle.variable("Moisture", moisture); //cloud variable that reads moisture data
+  Particle.function("Water", water); // cloud function to water the plant
 }
 
-// loop() runs over and over again, as quickly as it can execute.
+
 void loop() {
   moistureData = analogRead(moistureSensor);
 
+  moisture = moistureData;
+  
+  delay(5000); //Only read data every 5s; moisture levels won't cahnge rapidly
 }
 
-void water(String input){
-  
-
-
+//Function for watering the plant
+int water(String input){
+  if(input == "Water"){
+    digitalWrite(pump, HIGH);
+    delay(3000);
+    digitalWrite(pump, lOW);
+  }
+  else{
+    digitalWrite(pump, LOW);
+  }
+  return 0;
 }
